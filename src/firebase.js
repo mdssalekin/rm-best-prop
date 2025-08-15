9fb615ee80ba5b29e0145ef2503bc24bdfca78a1
salekin <mohammad.shirajus.salekin@gmail.com>
2025-08-15 06:19:51 +0600

    upgraded firebase

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIRIBASE_API_KEY ,
-  authDomain: "property-sell-401819.firebaseapp.com",
-  projectId: "property-sell-401819",
-  storageBucket: "property-sell-401819.appspot.com",
-  messagingSenderId: "588716927912",
-  appId: "1:588716927912:web:a6d64c89172800b05a4b9d",
-  measurementId: "G-NQ79YSEFBT"
+  apiKey: "AIzaSyCv8Yyxj0EA6ylGz0Ht5zV495IwqnrLdWk",
+  authDomain: "rm-best-properties.firebaseapp.com",
+  projectId: "rm-best-properties",
+  storageBucket: "rm-best-properties.firebasestorage.app",
+  messagingSenderId: "910952660182",
+  appId: "1:910952660182:web:2a13e187565901623e1d77",
+  measurementId: "G-6NHBN30N0B"
 };
 
 // Initialize Firebase
